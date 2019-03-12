324db0e05cd582e7d517d72c38581ec43379fccb
marcoortu <=>
2019-03-12 14:27:09 +0100

    moving code to main.py

--- booleanModel.py
+++ booleanModel.py
@@ -54,28 +54,3 @@ def match(query, negation=False, docs=docs):
     if negation:
         return set([k for k in tokens.keys() if query.lower() not in tokens[k]])
     return set([k for k in tokens.keys() if query.lower() in tokens[k]])
-
-
-
-
-
-
-if __name__ == "__main__":
-    print "Using Fufunctions:"
-    print match("recupero") & match("web")
-    print match("recupero") | match("web")
-    print match("recupero") & match("relitto", negation=True)
-    print (match("web") | match("uso")) & match("strumenti")
-    print (match("uso") | match("web")) & match("strumenti", negation=True)
-    print match("informazioni") & match("relitto") & match("studente")
-    print match("informazioni") | match("relitto") | match("studente")
-    print match("bologna") | match("padova", negation=True)
-    print "Using Objects:"
-    print BooleanModel("recupero") & BooleanModel("web")
-    print BooleanModel("recupero") | BooleanModel("web")
-    print BooleanModel("recupero") & ~BooleanModel("relitto")
-    print (BooleanModel("web") | BooleanModel("uso")) & BooleanModel("strumenti")
-    print (BooleanModel("uso") | BooleanModel("web")) & ~BooleanModel("strumenti")
-    print BooleanModel("informazioni") & BooleanModel("relitto") & BooleanModel("studente")
-    print BooleanModel("informazioni") | BooleanModel("relitto") | BooleanModel("studente")
-    print BooleanModel("bologna") | ~BooleanModel("padova")
