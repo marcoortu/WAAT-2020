0ae5ac7a5f9058249d1f9cbd52e4e0b8d0b65fe5
marcoortu <=>
2019-03-12 14:25:16 +0100

    clean up

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-from vectorModel import rank
-
 if __name__ == "__main__":
-    useTfIdf = True
-    print rank(query="spada oggetto", useTfIdf=useTfIdf)
-    print rank(query="un che", useTfIdf=useTfIdf)
-    print rank(query="mattina sole", useTfIdf=useTfIdf)
+    print "hello world"
