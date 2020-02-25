dce098b0b9bf67c096a7cfe188d47e5f3a393e28
berserk <marco.ortu@diee.unica.it>
2020-02-25 18:54:00 +0100

    clean up

---
+++ inverted_index.py
@@ -0,0 +1,132 @@
+import string
+
+translator = str.maketrans('', '', string.punctuation)
+
+doc1 = (
+    "doc1",
+    """una mattina, svegliandosi da sogni irrequieti Gregor Samsa
+    si trovo nel suo letto trasformato in un insetto mostruoso
+    """
+)
+
+doc2 = (
+    "doc2",
+    """Voi che trovate quando tornate a casa il cibo caldo e visi amici 
+    Considerate se questo è un uomo
+    """
+)
+
+doc3 = (
+    "doc3",
+    """Vidi un magnifico disegno rappresentava un serpente boa 
+    nell atto di inghiottire un animale
+    """
+)
+
+doc4 = (
+    "doc4",
+    """animale Vidi un magnifico disegno rappresentava un serpente boa 
+    nell atto di inghiottire un 
+    """
+)
+
+
+class InvertedIndex(object):
+
+    def __init__(self, docs):
+        self.inverted_indexes = {}
+        for doc in docs:
+            index = self.get_index_occurence(doc)
+            self.update(index, doc[0])
+
+    def update(self, indexes, doc_name):
+        for index in indexes:
+            if index[0] in self.inverted_indexes.keys():
+                self.inverted_indexes[index[0]].append((doc_name, index[1]))
+            else:
+                self.inverted_indexes[index[0]] = [(doc_name, index[1])]
+
+    def __str__(self):
+        return str(self.inverted_indexes)
+
+    def __iter__(self):
+        return iter(self.inverted_indexes.items())
+
+    def get_index_occurence(self, doc):
+        words = doc[1].split()
+        words = [word.translate(translator) for word in words]
+        index = []
+        for i, word in enumerate(words):
+            index.append((word, i + 1))
+        return index
+
+    def find(self, query=[]):
+        docs = []
+        for term in query:
+            if term in self.inverted_indexes.keys():
+                docs.append(set([t[0] for t in self.inverted_indexes[term]]))
+        return list(set.intersection(*docs))
+
+    def find_sequential(self, query=[]):
+        docs = self.find(query)
+        doc_dict = {}
+        retrieved_docs = []
+        for term in query:
+            if term in self.inverted_indexes.keys():
+                for t in [t for t in self.inverted_indexes[term] if t[0] in docs]:
+                    if t[0] in doc_dict.keys():
+                        doc_dict[t[0]].append(t[1])
+                    else:
+                        doc_dict[t[0]] = [t[1]]
+        for key in doc_dict.keys():
+            doc_dict[key] = sorted(doc_dict[key])
+            doc_dict[key] = [abs(doc_dict[key][i] - doc_dict[key][i + 1])
+                            for i in range(0, len(doc_dict[key]) - 1)]
+            if 1 in doc_dict[key]:
+                retrieved_docs.append(key)
+        return retrieved_docs
+
+
+def geti_index_occurence(doc):
+    words = doc[1].split()
+    words = [word.translate(translator) for word in words]
+    return [(word, i + 1) for i, word in enumerate(words)]
+
+
+def create_inverted_index(docs=[]):
+    inverted_index = {}
+    for doc in docs:
+        for index in geti_index_occurence(doc):
+            if index[0] in inverted_index.keys():
+                inverted_index[index[0]].append((doc[0], index[1]))
+            else:
+                inverted_index[index[0]] = [(doc[0], index[1])]
+    return inverted_index
+
+
+def find(inverted_index={}, query=[]):
+    docs = []
+    for term in query:
+        if term in inverted_index.keys():
+            docs.append(set([t[0] for t in inverted_index[term]]))
+    return list(set.intersection(*docs))
+
+
+def find_sequential(inverted_index={}, query=[]):
+    docs = find(inverted_index, query)
+    doc_dict = {}
+    retrieved_docs = []
+    for term in query:
+        if term in inverted_index.keys():
+            for t in [t for t in inverted_index[term] if t[0] in docs]:
+                if t[0] in doc_dict.keys():
+                    doc_dict[t[0]].append(t[1])
+                else:
+                    doc_dict[t[0]] = [t[1]]
+    for key in doc_dict.keys():
+        doc_dict[key] = sorted(doc_dict[key])
+        doc_dict[key] = [abs(doc_dict[key][i] - doc_dict[key][i + 1])
+                        for i in range(0, len(doc_dict[key]) - 1)]
+        if 1 in doc_dict[key]:
+            retrieved_docs.append(key)
+    return retrieved_docs
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from invertedIndex import createInvertedIndex, find, findSequential, doc1, doc2, doc3, InvertedIndex
+from inverted_index import create_inverted_index, doc1, doc2, doc3, find, find_sequential, InvertedIndex
 
 if __name__ == "__main__":
     # With Functions
-    invertedIndex = createInvertedIndex(docs=[doc1, doc2, doc3])
-    print(find(invertedIndex, ["un", "atto"]))
-    print(find(invertedIndex, ["un", "animale"]))
-    print(findSequential(invertedIndex, ["animale", "un"]))
+    inv_indexes = create_inverted_index(docs=[doc1, doc2, doc3])
+    print(find(inv_indexes, ["un", "atto"]))
+    print(find(inv_indexes, ["un", "animale"]))
+    print(find_sequential(inv_indexes, ["animale", "un"]))
 
     # With Classes
-    invertedIndex = InvertedIndex(docs=[doc1, doc2, doc3])
-    print(invertedIndex.find(["un", "atto"]))
-    print(invertedIndex.find(["un", "animale"]))
-    print(invertedIndex.findSequential(["animale", "un"]))
+    inv_indexes = InvertedIndex(docs=[doc1, doc2, doc3])
+    print(inv_indexes.find(["un", "atto"]))
+    print(inv_indexes.find(["un", "animale"]))
+    print(inv_indexes.find_sequential(["animale", "un"]))
