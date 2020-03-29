f43a1bc0cd5c5b85c5650ee0cca749fe3d198c6a
marcoortu <marco.ortu@diee.unica.it>
2020-03-29 12:47:52 +0200

    solution

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from exercise1 import get_adjectives
 from exercise2 import AuthorSentimentAnalyzer
 
 if __name__ == '__main__':
+    # Exercise 1
     print(get_adjectives('https://www.ft.com/'))
     print(get_adjectives('https://www.economist.com/'))
 
+    # Exercise 2
     clf = AuthorSentimentAnalyzer()
     print(clf.get_sentiment('deledda'))
     print(clf.get_sentiment('pirandello'))
