8495a6fca341597dcc910bb4ce45d0dedb2a839d
marcoortu <marco.ortu@diee.unica.it>
2020-03-29 12:47:28 +0200

    solution

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    print("hello world")
+from exercise1 import get_adjectives
+
+from exercise2 import AuthorSentimentAnalyzer
+
+if __name__ == '__main__':
+    print(get_adjectives('https://www.ft.com/'))
+    print(get_adjectives('https://www.economist.com/'))
+
+    clf = AuthorSentimentAnalyzer()
+    print(clf.get_sentiment('deledda'))
+    print(clf.get_sentiment('pirandello'))
