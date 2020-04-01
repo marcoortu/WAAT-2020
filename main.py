5a6837f91defb0550dc59d9b13a2c0916463ef53
marcoortu <marco.ortu@diee.unica.it>
2020-04-01 17:40:50 +0200

    speech recognition example with Google API

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    print("hello world")
+import speech_recognition as sr
+
+if __name__ == '__main__':
+    r = sr.Recognizer()
+    harvard = sr.AudioFile('./data/OSR_us_000_0010_8k.wav')
+    with harvard as source:
+        audio = r.record(source)
+        google_text = r.recognize_google(audio)
+        sphinx_text = r.recognize_sphinx(audio)
+        print(google_text)
+        print(sphinx_text)
