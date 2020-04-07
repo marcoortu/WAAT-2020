da3b7eae639db9cf2685943d97251ea2b32e4e2c
marcoortu <marco.ortu@diee.unica.it>
2020-04-07 09:16:30 +0200

    SVM non linear kernel

--- svm.py
+++ svm.py
@@ -1,7 +1,9 @@
 import random
 import re
 import string
+import warnings
 
+from matplotlib.colors import ListedColormap
 from nltk import PorterStemmer, WordNetLemmatizer, word_tokenize
 from nltk.corpus import movie_reviews
 from nltk.corpus import wordnet as wn, stopwords
@@ -15,6 +17,90 @@ DOMAIN_STOP_WORDS = """
 trunk build commit branch patch
 release bug regression close fix
 """
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.6,
+                    c=cmap(idx),
+                    edgecolor='black',
+                    marker=markers[idx],
+                    label=cl)
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
+            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
+            warnings.warn('Please update to NumPy 1.9.0 or newer')
+        else:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    alpha=1.0,
+                    edgecolor='black',
+                    linewidths=1,
+                    marker='o',
+                    s=55, label='test set')
+
+
+def non_linear_classes():
+    np.random.seed(0)
+    X_xor = np.random.randn(200, 2)
+    y_xor = np.logical_xor(X_xor[:, 0] > 0,
+                           X_xor[:, 1] > 0)
+    y_xor = np.where(y_xor, 1, -1)
+
+    plt.scatter(X_xor[y_xor == 1, 0],
+                X_xor[y_xor == 1, 1],
+                c='b', marker='x',
+                label='1')
+    plt.scatter(X_xor[y_xor == -1, 0],
+                X_xor[y_xor == -1, 1],
+                c='r',
+                marker='s',
+                label='-1')
+
+    plt.xlim([-3, 3])
+    plt.ylim([-3, 3])
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.show()
+    # Using a non linear kernel
+    svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+    svm.fit(X_xor, y_xor)
+    plot_decision_regions(X_xor, y_xor,
+                          classifier=svm)
+
+    plt.legend(loc='upper left')
+    plt.tight_layout()
+    plt.show()
 
 
 def domain_stop_words():
@@ -37,7 +123,7 @@ class LemmaTokenizer(object):
                 wn.synsets(t)]
 
 
-if __name__ == '__main__':
+def pipe_example():
     classifier = Pipeline([
         ('feature_vect', TfidfVectorizer(strip_accents='unicode',
                                          tokenizer=word_tokenize,
@@ -69,3 +155,7 @@ if __name__ == '__main__':
     print(accuracy_score(yTest, predicted))
     print(precision_recall_fscore_support(yTest, predicted))
     print(classification_report(yTest, predicted))
+
+
+if __name__ == '__main__':
+    non_linear_classes()
