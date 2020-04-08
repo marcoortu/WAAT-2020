21d41428417355030ea10853912cbc4620e55c76
marcoortu <marco.ortu@diee.unica.it>
2020-04-08 09:06:04 +0200

    clean up

--- classification.py
+++ classification.py
@@ -52,7 +52,7 @@ def movie_review_features(document, word_features):
     document_words = set(document)
     features = {}
     for word in word_features:
-        features['contains({})'.format(word)] = (word in document_words)
+        features[word] = word in document_words
     return features
 
 
