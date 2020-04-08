e1bb49ff31a02412b85a716d981930f039694fd2
marcoortu <marco.ortu@diee.unica.it>
2020-04-08 09:12:39 +0200

    web analytics example

---
+++ www/website/js/customjs.js
@@ -0,0 +1,10 @@
+function download() {
+    _gaq.push([
+        '_trackEvent',
+        'download',
+        'product_download',
+        "product_download",
+        11
+    ]);
+    alert('Thank you!');
+}
